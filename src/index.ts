2eee768b470bab184a6e46aba716855bc156992b
Ivan Posazhennikov <ivanposazhennikov99@gmail.com>
2023-07-25 21:44:04 +0300

    fix: fix export plugin name to the CapacitorHeicConverter

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { HeicConverterPlugin } from './definitions';
 
-const HeicConverter = registerPlugin<HeicConverterPlugin>('HeicConverter', {
+const CapacitorHeicConverter = registerPlugin<HeicConverterPlugin>('HeicConverter', {
   web: () => import('./web').then(m => new m.HeicConverterWeb()),
 });
 
 export * from './definitions';
-export { HeicConverter };
+export { CapacitorHeicConverter };
